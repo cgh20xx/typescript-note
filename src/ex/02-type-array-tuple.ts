ba6a83fd2fa2901210f519244bebceee9d158179
Hank <cgh20xx@gmail.com>
2023-04-27 17:06:22 +0800

    feat: 新增元組 tuple 的宣告方式

--- src/ex/02-type-array-tuple.ts
+++ src/ex/02-type-array-tuple.ts
@@ -1,6 +1,10 @@
 /**
- * 陣列與元祖
+ * 陣列 array: 陣列元素沒有順序性，類型對了即可。
+ * 元祖 tuple: 陣列元素類型有順序性且數量要一致。
+ * 
+ * 陣列及元組的宣告方式不可混用
  */
+
 
 // 單一元素陣列
 const arr1: number[] = [1, 2, 3];
@@ -17,3 +21,14 @@ const arr3: (number | string)[] = [1, 2, '3']
 const arr4: Array<number | string> = [1, 2, '3']
 // 多元素陣列 寫法3:(自動推導)
 const arr5 = [1, 2, '3'] // 自動推導為 (string | number)[]
+// 單一元素2維陣列
+const arr6: number[][] = [[1], [1, 2], [1, 2, 3]];
+// 多元素2維陣列
+const arr7: (number | string)[][] = [[1], ['2', '2'], [1, 2, '3']];
+// 多元素2維陣列 (實際應該不太會這樣寫)
+const arr8: ((number)[]| string)[] = [[1], [2, 2], '3'];
+
+// Tuple 元組
+const tu1: [number, string, boolean] = [123, '456', true];
+// Tuple 元組的陣列包陣列
+const tu2: [number, number][] = [[1, 2], [3, 4]];
