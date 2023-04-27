a51f8a4405af7bbbaaf9262a2f4b9da79f8217e1
Hank <cgh20xx@gmail.com>
2023-04-27 17:40:42 +0800

    feat: 新增物件及屬性的型別

---
+++ src/ex/03-type-object.ts
@@ -0,0 +1,17 @@
+/**
+ * 物件及屬性的型別
+ * 屬性後面加?表示可選
+ */
+let obj: { name: string, age?: number }
+
+// 以下這樣是合法的
+obj = {
+  name: 'Hank',
+  age: 99
+}
+
+// 以下這樣是合法的, age 為可省略的屬性
+obj = {
+  name: 'Hank',
+}
+
