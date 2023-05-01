6256141365f6bd8e3095fe8e37d34cbb621de21d
Hank <cgh20xx@gmail.com>
2023-05-01 16:51:44 +0800

    feat: 新增泛型 generics 範例

---
+++ src/ex/11-function-generics.ts
@@ -0,0 +1,33 @@
+// 泛型 Generics
+// 使用 < > 符號包住
+// 將函式參數的型別決定權，交由外部使用時才決定要傳入什麼型別。
+
+// T 表示 type 的意思，可以是任何自訂的名稱，只是大家習慣用 T。
+
+function print<T>(data: T) {
+  console.log(data);
+}
+
+// 如果不使用泛型，就需要寫很多次
+// function print1(data: string) {
+//   console.log(data);
+// }
+
+// function print2(data: number) {
+//   console.log(data);
+// }
+
+// function print3(data: boolean) {
+//   console.log(data);
+// }
+
+// 使用 print 會自動推導傳入的參數型別
+print('hank')
+print(123)
+print(true)
+
+// 也可顯示的寫的泛型告訴傳入的參數型別
+print<string>('hank')
+print<number>(123)
+print<boolean>(true)
+
--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,5 @@
 // import './ex/07-interface-define'
 // import './ex/08-interface-vs-type'
 // import './ex/09-enum'
-import './ex/10-function-basic'
+// import './ex/10-function-basic'
+import './ex/11-function-generics'
