63b8758b5bdeb995660f41d6058d44ba7063f6f5
Hank <cgh20xx@gmail.com>
2023-05-03 20:37:46 +0800

    feat: 新增 class es6 private 範例

--- src/ex/17-class-private-protected.ts
+++ src/ex/17-class-private-protected.ts
@@ -4,6 +4,8 @@
 // protected：受保護的，只有自己內部和繼承的子類別內部可以存取。
 // private：私有的，只有自己內部可以存取。
 
+// 注意：protected、private 只有在 ts 開發中才會有效，輸出的 js 還是 public。
+
 class BaseInfo {
   // public name: string = 'hank'
   protected name: string = 'hank'
@@ -24,3 +26,18 @@ const baseinfo = new BaseInfo();
 const userInfo = new UserInfo();
 // console.log(userInfo.name); // error: protected name 不可直接存取
 console.log(userInfo.getName());
+
+
+// js 原生 Private fields
+// 注意：tsconfig.js compilerOptions.target 需設定為 ES6 以上
+
+// 做用 # 符號表示 private
+class PrivateInfo {
+  #name: string = 'private name'
+  getName() {
+    return this.#name;
+  }
+}
+
+const privateInfo = new PrivateInfo();
+console.log('privateInfo:', privateInfo);
