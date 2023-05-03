0a17b044f87731d57cad65e5ea4320019c268ab2
Hank <cgh20xx@gmail.com>
2023-05-03 17:03:17 +0800

    feat: 新增 class constructor super 範例

--- src/ex/16-class-extends.ts
+++ src/ex/16-class-extends.ts
@@ -1,23 +1,47 @@
 // -------- 繼承類別 --------
 
+// extend
 class Animal2 {
   name: string
   run() {
-    console.log('animal run');
+    console.log('animal2 run');
   }
 }
 
 class Dog2 extends Animal2 {
   run() {
-    console.log('dog run');
+    console.log('dog2 run');
   }
 }
 
-class Cat2 extends Animal2 {
+const d2= new Dog2();
+d2.run();
 
+// constructor & super
+class Animal3 {
+  name: string
+  constructor(name: string) {
+    console.log('Animal3 constructor');
+    this.name = name;
+  }
+  run() {
+    console.log('animal3 run');
+  }
 }
 
-const d1 = new Dog2();
-d1.run();
-const c1 = new Cat2();
-c1.run();
+class Dog3 extends Animal3 {
+  // 注意：子類別如果沒寫 constructor 會直接呼叫父層的 constructor
+
+  // override 父層的 constructor
+  constructor(name: string) {
+    super(name) // 一定要呼叫 super 否則 ts 會報錯
+  }
+  run() {
+    super.run(); // 呼叫父層的方法
+    console.log(`${this.name} run`);
+  }
+}
+
+const d3 = new Dog3('juby3');
+d3.run();
+// console.log('dog3:', d3);
