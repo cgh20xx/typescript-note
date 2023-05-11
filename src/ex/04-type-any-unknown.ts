633cab70d103a8cc7e6cdeda5a070d1ead6c088d
Hank <cgh20xx@gmail.com>
2023-05-11 17:29:18 +0800

    feat: 新增型別斷言（Type Assertion）範例

--- src/ex/04-type-any-unknown.ts
+++ src/ex/04-type-any-unknown.ts
@@ -8,26 +8,6 @@
 let name1: unknown = 'hank'
 name1 = 123
 
-// 斷言 Assertions ?
-// 使用 as 關鍵字來指定型別
-let name2: unknown = 'hank2'
-let name3 = name2 as string
-
-// 斷言 as 的應用
-async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  // 使用 as 關鍵字強制給 data 添加物件型別
-  const data = await res.json() as { 
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean
-  }
-  console.log(data)
-}
-
-getData()
-
 // any 與 unknown
 // 若使用 any 則可能沒辦法在編譯時期檢查到錯誤
 const flag = false
---
+++ src/ex/04-type-assertion.ts
@@ -0,0 +1,58 @@
+// ============ 型別斷言（Type Assertion） ============
+// 可以用來手動指定一個值的型別。
+// 語法1：值 as 型別
+// 語法2：<型別>值
+
+let name2: unknown = 'hank2'
+let name3 = name2 as string
+
+// 有時候，我們需要在還不確定型別的時候就訪問其中一個型別的屬性或方法，比如
+// function getLength(something: string | number): number {
+//   if (something.length) {
+//       return something.length;
+//   } else {
+//       return something.toString().length;
+//   }
+// }
+
+// index.ts(2,19): error TS2339: Property 'length' does not exist on type 'string | number'.
+//   Property 'length' does not exist on type 'number'.
+// index.ts(3,26): error TS2339: Property 'length' does not exist on type 'string | number'.
+//   Property 'length' does not exist on type 'number'.
+
+// 上例中，存取 something.length 的時候會報錯。
+// 此時可以使用型別斷言，將 something 斷言成 string：
+function getLength(something: string | number): number {
+  // 第一種寫法
+  if ((<string>something).length) {
+      return (<string>something).length;
+  } else {
+      return something.toString().length;
+  }
+  // 第二種寫法
+  // if ((something as string).length) {
+  //   return (something as string).length;
+  // } else {
+  //     return something.toString().length;
+  // }
+}
+let len = getLength('abcde')
+console.log('len abcde:', len);
+len = getLength(123)
+console.log('len 123:', len);
+
+
+// 斷言 as 的應用
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  // 使用 as 關鍵字強制給 data 添加物件型別
+  const data = await res.json() as { 
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+  }
+  console.log(data)
+}
+
+getData()
