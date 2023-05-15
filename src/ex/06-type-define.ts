5169f701778bdd21174f20c8c6ad4d13704645f8
Hank <cgh20xx@gmail.com>
2023-05-15 15:24:17 +0800

    feat: 新增 String Literal 字串字面量型別範例

--- src/ex/06-type-define.ts
+++ src/ex/06-type-define.ts
@@ -1,8 +1,6 @@
-/**
- * type: 型別別名
- * 用來給一個型別起個新名字。
- * type 常用於聯合型別。
- */
+// =========== type 型別別名 ===========
+// 用來給一個型別起個新名字。
+// type 常用於聯合型別。 
 
 // 基本型別
 type CustomType = string
@@ -33,5 +31,18 @@ people2 = {
 let people3 = {} as Person
 people3.name = 'tom'
 people3.age = 20
+
+// =========== String Literal 字串字面量型別 ===========
+// 字串字面量 (String Literal) 型別用來約束取值只能是某幾個字串中的一個。
+type EventNames = 'click' | 'scroll' | 'mousemove';
+function handleEvent(ele: Element | null, event: EventNames) {
+    // do something
+}
+
+handleEvent(document.getElementById('hello'), 'scroll');  // 沒問題
+// handleEvent(document.getElementById('world'), 'dbclick'); // 報錯，event 不能為 'dbclick'
+
+// index.ts(7,47): error TS2345: Argument of type '"dbclick"' is not assignable to parameter of type 'EventNames'.
+
 
 export {}
