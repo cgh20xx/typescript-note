fbddcd2a0772173d72ddccef470a21b04a39a109
Hank <cgh20xx@gmail.com>
2023-05-16 11:34:11 +0800

    feat: 新增可實作多個介面範例，介面繼承介面範例。

--- src/ex/15-class-interface.ts
+++ src/ex/15-class-interface.ts
@@ -54,4 +54,39 @@ console.log('animal:',animal);
 animal.walk(2)
 console.log('makeSound:', animal.makeSound());
 
+// 介面可以繼承介面
+class Point {
+  x: number;
+  y: number;
+}
+
+interface Point3d extends Point {
+  z: number;
+}
+
+let point3d: Point3d = {x: 1, y: 2, z: 3};
+
+
+// 一個類別可以實現多個介面：(用逗號隔開)
+interface Alarm {
+  alert(): void;
+}
+
+interface Light {
+  lightOn(): void;
+  lightOff(): void;
+}
+
+class Car implements Alarm, Light {
+  alert() {
+      console.log('Car alert');
+  }
+  lightOn() {
+      console.log('Car light on');
+  }
+  lightOff() {
+      console.log('Car light off');
+  }
+}
+
 export {}
