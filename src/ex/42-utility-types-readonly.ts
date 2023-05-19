b946c405fe9da14dae1e04ca405c082fa4d7aa91
Hank <cgh20xx@gmail.com>
2023-05-19 14:11:35 +0800

    feat: 新增 utility types Readonly

---
+++ src/ex/42-utility-types-readonly.ts
@@ -0,0 +1,13 @@
+// Readonly<T>：將型別 T 的所有屬性變為只讀。
+
+interface Todo {
+  title: string;
+}
+ 
+const todo: Readonly<Todo> = {
+  title: "Delete inactive users",
+};
+ 
+todo.title = "Hello"; // error: Cannot assign to 'title' because it is a read-only property.
+
+export {}
