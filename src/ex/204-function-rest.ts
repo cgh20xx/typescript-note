780d251ed068a7cd0e08ed459b42bdf1dd58b14e
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-02-29 12:16:08 +0800

    feat: 新增 function Rest 參數的說明及 as const 用於物件範例

--- src/ex/204-function-rest.ts
+++ src/ex/204-function-rest.ts
@@ -1,4 +1,5 @@
-// function Rest 參數
+// ========== function Rest 參數 ==========
+// 其餘參數必需為元組類型或傳遞給一個 rest 參數。
 
 // 多個參數
 function calculate1(...nums: number[]) {
@@ -18,12 +19,23 @@ const nums2 = [1, 2, 3];
 // calculate2(...nums2); // error: A spread argument must either have a tuple type or be passed to a rest parameter.
 
 // 以下皆正常：
-// const nums2 = [1, 2, 3] as const;
+const nums3 = [1, 2, 3] as const;
+calculate2(...nums3);
 // 什麼是 as const:
 // 官方說明：The as const suffix acts like const but for the type system, ensuring that all properties are assigned the literal type instead of a more general version like string or number.
 // chat-gpt：當你使用 as const 斷言時，TypeScript 會將所有的字面量類型（例如字符串、數字、布爾值、陣列等）都轉換為對應的「字面量類型」，而不是一般的「變量類型」。使用 as const 斷言時，TypeScript 會自動推斷出適當的類型，並且將它們轉換為常量。這樣做可以讓 TypeScript 編譯器進行更嚴格的類型檢查，避免出現一些潛在的錯誤。
 
-// const nums2 = [1, 2, 3] as [number, number, number];
-// calculate2(...nums2);
+const nums4 = [1, 2, 3] as [number, number, number];
+calculate2(...nums4);
+
+// 補充：as const 用於物件，會將物件的屬性都轉換為 readonly
+// 型別：const people: {
+//   readonly name: "hank";
+//   readonly age: 40;
+// }
+const people = {
+  name: 'hank',
+  age: 40,
+} as const;
 
 export {};
