59c23b3cc6a6950569c7c7be520f14a921a2efb4
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-03-02 21:27:18 +0800

    feat: 新增泛型類別的擴充範例

--- src/ex/401-generics-class.ts
+++ src/ex/401-generics-class.ts
@@ -18,4 +18,52 @@ console.log(sum);
 // 推斷型別：GenericNumber<unknown>
 let myGenericUnknown = new GenericNumber();
 
+// ========== 擴充泛型類別 extends Class ============
+// TypeScript 不會嘗試從使用的情況中推薦基本類別(父類別)的引數型別。
+// 任何沒有預設值的引數型別，都需要使用一個明確的型別註記來指定。
+
+class Quote<T> {
+  lines: T;
+  constructor(line: T) {
+    this.lines = line;
+  }
+}
+
+new Quote('hello world').lines; // 型別：string
+new Quote([1, 2, 3]).lines; // 型別：number[]
+
+// 正確範例：擴充泛型類別明確指定型別(但子類別以後就只能接受固定的型別)
+class SpokenQuote extends Quote<string[]> {
+  speak() {
+    console.log(this.lines.join('\n'));
+  }
+}
+
+// 錯誤範例：擴充泛型類別未明確指定型別
+// 錯誤：Generic type 'Quote<T>' requires 1 type argument(s).ts(2314)
+// class SpokenQuote extends Quote {
+//   speak() {
+//     // 錯誤：Property 'lines' does not exist on type 'SpokenQuote'.ts(2339)
+//     console.log(this.lines.join('\n'));
+//   }
+// }
+
+new SpokenQuote(['hello', 'world']).lines; // 型別：string[]
+// new SpokenQuote([1, 2, 3]).speak(); // 錯誤：Type 'number' is not assignable to type 'string'.ts(2322)
+
+// 泛型衍生類別(子類別)可以將它自的引數類型傳遞給基本類別(父類別)，
+// 型別名稱不需要相同，這邊用 T2 來表示，避免混淆，要用 T 也是可以的。
+class AttributeQuote<T2> extends Quote<T2> {
+  speaker: string;
+  constructor(line: T2, speaker: string) {
+    super(line);
+    this.speaker = speaker;
+  }
+}
+
+new AttributeQuote('hello world', 'hank').lines; // 型別：string
+new AttributeQuote(123, 'hank').lines; // 型別：number
+new AttributeQuote([1, 2, 3], 'hank').lines; // 型別：number[]
+// 這樣子類別就可讓 TypeScript 自動推導出屬性的型別，不需要再手動指定型別，跟上個例子 SpokenQuote 比，更為靈活。
+
 export {};
