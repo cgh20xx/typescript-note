f7494c1faabab60eb05bb1c07b8aeeb16bdc2780
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-03-02 20:55:59 +0800

    feat: 更新泛型類別的註解和範例程式碼

--- src/ex/401-generics-class.ts
+++ src/ex/401-generics-class.ts
@@ -1,10 +1,12 @@
-// class generics 泛型類別
+// ========== class generics 泛型類別 ============
+// 類別實體也可以透過明確的引數型別來避免預設為 unknown，就像其它泛型函數呼叫一樣。
 
 class GenericNumber<T> {
   zeroValue!: T;
   add!: (x: T, y: T) => T;
 }
 
+// 明確型別：GenericNumber<number>
 let myGenericNumber = new GenericNumber<number>();
 myGenericNumber.zeroValue = 0;
 myGenericNumber.add = function (x, y) {
@@ -12,5 +14,8 @@ myGenericNumber.add = function (x, y) {
 };
 let sum = myGenericNumber.add(2, 3);
 console.log(sum);
+
+// 推斷型別：GenericNumber<unknown>
+let myGenericUnknown = new GenericNumber();
 
 export {};
