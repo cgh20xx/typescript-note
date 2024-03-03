c4961dd29e4ffb988ad53033135138f4a4cc531d
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-03-03 16:48:10 +0800

    feat: 新增泛型可辨識的聯集範例

--- src/ex/501-object-union.ts
+++ src/ex/501-object-union.ts
@@ -33,3 +33,34 @@ poem.type; // "pages" | "rhymes"
 
 // 未經過型別窄化，無法存取 pages 屬性
 // poem.pages; // Error: Property 'pages' does not exist on type 'PoemWithRhymes'.
+
+// ========= 泛型可辨識的聯集 =========
+// 此例 Result 泛型型別具有一個 succeeded 屬性，用來辨識成功或失敗的狀態，
+// 並需使用判斷式來進行型別窄化，限縮為成功或失敗。
+
+type Result<Data> = FailureResult | SuccessfulResult<Data>;
+
+type SuccessfulResult<Data> = {
+  succeeded: true;
+  data: Data;
+};
+
+type FailureResult = {
+  succeeded: false;
+  error: Error;
+};
+
+function handleResult(result: Result<string>) {
+  if (result.succeeded) {
+    // result 型別被窄化為 SuccessfulResult<string>
+    console.log(result.data);
+  } else {
+    // result 型別被窄化為 FailureResult
+    console.error(result.error);
+  }
+
+  // 未窄化就直接存取 data 屬性，會出現 2 個錯誤
+  // result.data
+  // 錯誤：Property 'data' does not exist on type 'Result<string>'.
+  // 錯誤：Property 'data' does not exist on type 'FailureResult'.
+}
