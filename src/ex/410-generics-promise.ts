3485c3afd813f7dab6656a9428c6e28a29d89f10
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-03-06 20:03:44 +0800

    feat: 新增非同步函數和回傳型別的說明

--- src/ex/410-generics-promise.ts
+++ src/ex/410-generics-promise.ts
@@ -26,3 +26,32 @@ const resolvedString = new Promise<string>((resolve) => {
 
 // 型別：Promise<number>
 const resolvedNumber = resolvedString.then((text) => text.length);
+
+// ========== 非同步函數 ==========
+// 在 JS 中使用 async 關鍵字宣告的函數會回傳 Promise，
+// 如果 JS 中的非同步函數回傳的數值不具有 Then 能力(即具有 then 方法)，則會被包裝成 Promise。
+// TS 瞭解這一點，並推斷非同步函數，無論回傳什麼數值，型別始終是 Promise。
+
+// 型別：function lengthAfterSecond(text: string): Promise<number>
+async function lengthAfterSecond(text: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return text.length;
+}
+
+// 型別：function lengthImmediately(text: string): Promise<number>
+async function lengthImmediately(text: string) {
+  return text.length;
+}
+
+// 因此，在任何 async 的函數上，手動宣告的回傳型別都必須是 Promise，即時該函數在其定義中沒有明確提及 Promise。
+
+// 正確
+async function givesPromiseForString(): Promise<string> {
+  return 'Done!';
+}
+
+// 錯誤：
+// async function givesString(): string {
+//   return 'Done!';
+// }
+// The return type of an async function or method must be the global Promise<T> type. Did you mean to write 'Promise<string>'?ts(1064)
