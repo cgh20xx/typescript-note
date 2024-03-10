25290b2a77a317a49c9ba826f612fef1958f68ad
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-03-10 16:30:16 +0800

    feat: 新增 705 模組宣告 declare module 範例

---
+++ src/ex/705-declare-module.ts
@@ -0,0 +1,10 @@
+// ========== 模組宣告 declare module ==========
+// 可以在模組名稱的字串之前使用 declare 關鍵字，告告知型別系統該模組的內容。
+
+// module.d.ts
+// declare module "my-example-lib" {
+//   export const someValue: number;
+// }
+
+// index.ts
+// import { someValue } from "my-example-lib";
