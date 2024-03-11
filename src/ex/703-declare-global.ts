891f2b342b6a19509a2991f72113e675aa82a4b5
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2024-03-12 00:00:48 +0800

    docs: 修正全域變數的註解

--- src/ex/703-declare-global.ts
+++ src/ex/703-declare-global.ts
@@ -1,5 +1,5 @@
 // ========== 使用 global 全域變數 ==========
 
-// 這裡 version 是全域變數，所以不用 import 就可以直接使用
+// 這裡 version 是全域變數，所以不用 import 也不會報錯，但實際在執行的時候，還是要能呼叫的到它才行。
 // 因為在 703-declare-global1.d.ts 中已經宣告過了
 console.log(version);
